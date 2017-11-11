51eade8c83afe4fe93ce0f4c708e23aea0ff2e80
henockbelete <etyeheny@gmail.com>
2017-11-11 17:54:27 +0100

    Generate Controller Song and Display list of Songs

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  get 'songs/index'
+
   get 'artists/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "artists#index"
-
+  root "songs#index"
 
   get "artists" => "artists#index"
+  get "songs" => "songs#index"
 
-  
+
 end
